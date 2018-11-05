072f1de2c166a329886dc1eb60ad3efec7a0944a
GIT4U\Kyle.Lin <kyle.lin@git4u.com>
2018-11-05 15:42:08 +0800

    modify ChainDataService for CFP

--- PushData/PushData/Service/ChainDataService.cs
+++ PushData/PushData/Service/ChainDataService.cs
@@ -27,7 +27,7 @@ namespace PushData.Service
         {
             try
             {
-                var requestUrl = "bnnautodatainfo";
+                var requestUrl = "cfpautodatainfo";
                 var response = _client.GetAsync(requestUrl).Result;
                 if (response.IsSuccessStatusCode == false)
                 {
