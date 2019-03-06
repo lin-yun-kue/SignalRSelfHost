c57a4e11fee6c1fe279d8e4b48ac1beeb1ee9ca9
GIT4U\Kyle.Lin <kyle.lin@git4u.com>
2019-03-06 15:03:40 +0800

    tgc socket

--- PushData/PushData/Service/ChainDataService.cs
+++ PushData/PushData/Service/ChainDataService.cs
@@ -27,7 +27,7 @@ namespace PushData.Service
         {
             try
             {
-                var requestUrl = "aitcautodatainfo";
+                var requestUrl = "tgcautodatainfo";
                 var response = _client.GetAsync(requestUrl).Result;
                 if (response.IsSuccessStatusCode == false)
                 {
