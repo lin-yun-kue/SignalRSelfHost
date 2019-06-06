a4cd43b94853f56f4ed2a0ad11700341b288a31b
GIT4U\Kyle.Lin <kyle.lin@git4u.com>
2019-06-06 10:40:05 +0800

    api socket first commit

--- PushData/PushData/Service/ChainDataService.cs
+++ PushData/PushData/Service/ChainDataService.cs
@@ -27,7 +27,7 @@ namespace PushData.Service
 
             try
             {
-                var requestUrl = "cicautodatainfo";
+                var requestUrl = "autodatainfo?item=api";
                 var response = client.GetAsync(requestUrl).Result;
                 if (response.IsSuccessStatusCode == false)
                 {
